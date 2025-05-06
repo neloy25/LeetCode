65a8cc65155b25dab5ef863d96c685ce8be4f0d5
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-06 17:55:10 +0600

    1: Two Sum

---
+++ 1_Two_Sum.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int)); // Allocate memory for 2 indices
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0; // In case no pair is found
+    return NULL;
+}
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int target,returnSize;
+    scanf("%d", &target);
+
+    int* indices = twoSum(arr, n, target, &returnSize);
+    printf("%d %d",indices[0],indices[1]);
+    free(indices);
+    return 0;
+}
